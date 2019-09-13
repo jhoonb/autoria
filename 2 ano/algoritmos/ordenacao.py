29eb65927829f9605792d0bef2d471b6b42c221a
jhoonb <jpbanczek@gmail.com>
2019-09-13 00:25:55 -0400

    algoritmos ordenação
    insertion, selection
    
    unit test ok

--- 2 ano/algoritmos/ordenacao.py
+++ 2 ano/algoritmos/ordenacao.py
@@ -6,6 +6,12 @@ Escola Padre Constantino de Monte
 
 
 def bubble_sort(vetor):
+    '''
+    bubble_sort -- pior caso O(n^2)
+    wiki: https://pt.wikipedia.org/wiki/Bubble_sort
+    parametro(list): um vetor 
+    retorno (list): o vetor ordenado 
+    '''
     for i in vetor:
         indice = 0
         while indice <= len(vetor)-1:
@@ -16,11 +22,47 @@ def bubble_sort(vetor):
             indice += 1
     return vetor
 
+
 def insertion_sort(vetor):
-    pass
+    '''
+    insertion_sort -- pior caso O(n^2)
+    wiki: https://pt.wikipedia.org/wiki/Insertion_sort
+    parametro(list): um vetor 
+    retorno (list): o vetor ordenado 
+    '''
+    indice = 1
+    while indice < len(vetor):
+        chave = vetor[indice]
+        j = indice
+        while (j > 0) and (vetor[j-1] > chave):
+            vetor[j] = vetor[j-1]
+            j -= 1
+        vetor[j] = chave 
+        indice += 1
+    return vetor
+
+
+
 
 def selection_sort(vetor):
-    pass 
+    '''
+    selection_sort -- pior caso O(n^2)
+    wiki: https://pt.wikipedia.org/wiki/Selection_sort
+    parametro(list): um vetor 
+    retorno (list): o vetor ordenado 
+    '''
+    indice = 0
+    while indice < len(vetor):
+        menor = indice
+        j = indice + 1
+        while j < len(vetor):
+            if vetor[j] < vetor[menor]:
+                menor = j
+            j += 1
+        vetor[indice], vetor[menor] = vetor[menor], vetor[indice]
+        indice += 1
+    return vetor
+
 
 def merge_sort(vetor):
     pass
--- 2 ano/algoritmos/test_ordenacao.py
+++ 2 ano/algoritmos/test_ordenacao.py
@@ -1,11 +1,31 @@
 import unittest
-from ordenacao import bubble_sort
+from ordenacao import (bubble_sort,
+    insertion_sort, selection_sort)
+
+"""
+py -m unittest -v test_ordenacao
+"""
+
 
 class TestOrdenacao(unittest.TestCase):
 
     def test_bubble_sort(self):
+        self.assertEqual(bubble_sort([1]), [1], "errado...")   
         self.assertEqual(bubble_sort([4, 3, 2, 1]), [1, 2, 3, 4], "errado...")   
         self.assertEqual(bubble_sort([101, 1, 45, 55, 67, 2, 3, 4]), [1, 2, 3, 4, 45, 55, 67, 101], "errado...") 
         self.assertEqual(bubble_sort(list(range(1000, -1,-1))), list(range(0, 1001)), "errado...") 
+
+    def test_insertion_sort(self):
+        # insertion
+        self.assertEqual(insertion_sort([4, 3, 2, 1]), [1, 2, 3, 4], "errado...")   
+        self.assertEqual(insertion_sort([101, 1, 45, 55, 67, 2, 3, 4]), [1, 2, 3, 4, 45, 55, 67, 101], "errado...") 
+        self.assertEqual(insertion_sort(list(range(1000, -1,-1))), list(range(0, 1001)), "errado...")
+
+    def test_selection_sort(self):
+        # selection
+        self.assertEqual(selection_sort([4, 3, 2, 1]), [1, 2, 3, 4], "errado...")   
+        self.assertEqual(selection_sort([101, 1, 45, 55, 67, 2, 3, 4]), [1, 2, 3, 4, 45, 55, 67, 101], "errado...") 
+        self.assertEqual(selection_sort(list(range(1000, -1,-1))), list(range(0, 1001)), "errado...") 
+
 if __name__ == '__main__':
     unittest.main()
